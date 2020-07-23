d6d2d9dbdee536b83c31449242f74d802995f68b
jaywcjlove <398188662@qq.com>
2020-07-23 17:57:10 +0800

    test: Update test case.

--- test/test.js
+++ test/test.js
@@ -5,3 +5,21 @@ test("when which found our program", async (t) => {
   let where = await whereis('ls');
   t.equal(where, '/bin/ls', 'ls was found');
 });
+
+test("undefined", async (t) => {
+  try {
+    await whereis();
+  } catch (error) {
+    t.equal(error, 'Could not find undefined on your system; Command failed: which undefined\n', 'undefined was found');
+    t.equal(error.message, undefined, 'undefined was found');
+  }
+});
+
+test("Empty", async (t) => {
+  try {
+    await whereis('');
+  } catch (error) {
+    t.equal(error, 'Could not find  on your system; Command failed: which \nusage: which [-as] program ...\n', 'Empty was found');
+    t.equal(error.message, undefined, 'Empty was found');
+  }
+});
